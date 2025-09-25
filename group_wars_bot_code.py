29eb5f6de0968b974d15a36e621aaa98027cbd17
AradSalehi <arad.student@outlook.com>
2025-09-25 09:53:30 +0330

    Edited first menu

--- group_wars_bot_code.py
+++ group_wars_bot_code.py
@@ -1,5 +1,5 @@
 import telebot
-
+from telebot import types
 
 BOT_TOKEN = "7955283951:AAHZ8rvCERvk48sHuU-F2u_hP_z6QZZq0bE"
 bot = telebot.TeleBot (BOT_TOKEN)
@@ -11,10 +11,11 @@ ADMINS_ID = ["6237975392" , "6391649892"]
 @bot.message_handler (commands =["start"])
 def start (message) :
     print (message.chat.id)
-    if str(message.chat.id) in ADMINS_ID :
-        bot.send_message (message.chat.id , "Admin detected.")
-    else :
-        bot.send_message (message.chat.id , "Normal user detected.")
-
+    markup = types.ReplyKeyboardMarkup (resize_keyboard=True)
+    markup.row("Shop" , "Exchange")
+    markup.row("Inventory" , "Status")
+    markup.row("Mines" , "Engage")
+    markup.row("help")
+    bot.send_message (message.chat.id , "Choose an option:" , reply_markup=markup)
 print("Bot is running...")
 bot.polling(non_stop=True)
