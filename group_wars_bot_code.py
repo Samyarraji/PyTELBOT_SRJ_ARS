276c15d75409bbf0907438b65fd731963aa77489
AradSalehi <arad.student@outlook.com>
2025-09-26 21:51:20 +0330

    Add help

--- group_wars_bot_code.py
+++ group_wars_bot_code.py
@@ -11,10 +11,10 @@ start_menu = types.ReplyKeyboardMarkup (resize_keyboard=True)
 start_menu.row("Shop 🛒" , "Exchange 💱")
 start_menu.row("Inventory 🎒" , "Status 📈")
 start_menu.row("Mines ⛏️" , "Engage 🪖")
-start_menu.row("help 🆘")
+start_menu.row("Help 🆘")
 
 #------------------
-#Shop
+#Shop </>
 #------------------
 
 shop_menu = types.ReplyKeyboardMarkup (resize_keyboard=True)
@@ -26,7 +26,7 @@ shop_menu.row("Defenses 🛡️")
 #------------------
 
 air_force_shop = types.ReplyKeyboardMarkup (resize_keyboard=True)
-air_force_shop.row("Aviator 🧑‍✈️")
+air_force_shop.row("Aviators 🧑‍✈️")
 air_force_shop.row("Fighter jets ✈️")
 air_force_shop.row("A-Munitions 🎇")
 
@@ -35,8 +35,8 @@ air_force_shop.row("A-Munitions 🎇")
 #------------------
 
 a_munitions_shop = types.ReplyKeyboardMarkup(resize_keyboard=True)
-a_munitions_shop.row("A-Bomb 💣")
-a_munitions_shop.row("A-Missile 🚀")
+a_munitions_shop.row("A-Bombs 💣")
+a_munitions_shop.row("A-Missiles 🚀")
 
 #------------------
 #Navy
@@ -64,6 +64,13 @@ defense_shop.row("Shield 🛡️")
 defense_shop.row("Defense system 🚩")
 
 #------------------
+#Help Markup
+#------------------
+
+help_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+help_markup.row("Information ❓" , "Contact us 💬") 
+
+#------------------
 #Menu Structure </>
 #------------------
 
@@ -74,6 +81,7 @@ MENUS = {"start": {"markup": start_menu, "parent": None},
     "Navy ⚓": {"markup": navy_shop, "parent": "Shop 🛒"},
     "B-Munitions 🎇": {"markup": b_munitions, "parent": "Navy ⚓"},
     "Defenses 🛡️": {"markup": defense_shop, "parent": "Shop 🛒"},
+    "Help 🆘" : {"markup": help_markup , "parent" : "start"}
     }
 
 def add_back (menu_name) :
@@ -90,6 +98,10 @@ user_state = {}
 
 @bot.message_handler(commands="start")
 def start(message) :
+    if message.chat.username :
+        print("User :",message.chat.id , "with username :" ,f"@{message.chat.username} has started the bot.")
+    else :
+        print("User :",message.chat.id , "with name :" ,f"{message.chat.first_name} has started the bot.")
     user_state[message.chat.id] = "start"
     bot.send_message(message.chat.id , "Choose an option:" , reply_markup=start_menu)
 
@@ -98,14 +110,34 @@ def menu_handler (message) :
     if message.text == "Back 🔙" :
         current_menu = user_state.get (message.chat.id , "start")
         parent = MENUS[current_menu]["parent"]
-        user_state [message.chat.id] = parent
-        bot.send_message(message.chat.id , "Choose an option:" , reply_markup=MENUS[parent]["markup"])
+        if parent :
+            user_state [message.chat.id] = parent
+            bot.send_message(message.chat.id , "Choose an option:" , reply_markup=MENUS[parent]["markup"])
+        else :
+            bot.send_message(message.chat.id , "❌ Invalid request.\nPlease start again using /start ")
     elif message.text in MENUS :
         user_state[message.chat.id] = message.text
         bot.send_message(message.chat.id , "Choose an option:" , reply_markup=MENUS[message.text]["markup"])
+    elif message.text == "Information ❓" :
+        bot.send_message(message.chat.id , "There is no information yet.")
+    elif message.text == "Contact us 💬" :
+        bot.send_message(message.chat.id , "Write your message down here 👇:")
+        bot.register_next_step_handler(message , help_message)
     else :
-        bot.send_message (message.chat.id , "❌ Invalid request.")
+        bot.send_message(message.chat.id , "❌ Invalid request.")
 
-    
+
+#------------------
+#Functions
+#------------------
+
+def help_message (message) :
+    for admin_id in ADMINS_ID :
+        if message.chat.username :
+            bot.send_message (admin_id , f"User: {message.chat.id} with @{message.chat.username} username said:\n\n{message.text}")
+        else :
+            bot.send_message(admin_id , f"User {message.chat.id} with {message.chat.first_name} username said:\n\n{message.text}")
+    bot.send_message(message.chat.id , "Your message has been sent successfully. ✅")
+
 print("Bot is running...")
 bot.polling(non_stop=True)
